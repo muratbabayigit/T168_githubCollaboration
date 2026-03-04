902e88a639488be82f3e74045543e99c69fb7d79
muratbabayigit <babayigit@gmial.com>
2026-03-04 21:54:10 +0300

    first commit

---
+++ src/Ortak.java
@@ -0,0 +1,5 @@
+public class Ortak {
+    public static void main(String[] args) {
+        System.out.println("buradab babayigit geçti");
+    }
+}
---
+++ src/babayigit.java
@@ -0,0 +1,5 @@
+public class babayigit {
+    public static void main(String[] args) {
+        System.out.println("Ne yaparsan yap aşk ile yap");
+    }
+}
