96755753c609b892bad8005f0d55fa0007bde2b8
muratbabayigit <babayigit@gmial.com>
2026-03-04 22:32:38 +0300

    hakikiversion1

--- src/babayigit.java
+++ src/babayigit.java
@@ -1,5 +1,6 @@
 public class babayigit {
     public static void main(String[] args) {
         System.out.println("Ne yaparsan yap aşk ile yap");
+        System.out.println("merhaba t168");
     }
 }
